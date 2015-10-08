d6eeb6f15573d4bbefa882394997437251f227ec
C.H.A.D.o <chado@chado.by>
2015-10-08 18:03:23 +0300

    Close #2

--- sql/monitoring.sql
+++ sql/monitoring.sql
@@ -1,13 +1,13 @@
 CREATE TABLE IF NOT EXISTS `monitoring` (
   `dttm` datetime NOT NULL COMMENT 'Время записи',
-  `programm` varchar(50) NOT NULL COMMENT 'Программа',
-  `worker` tinyint(3) NOT NULL DEFAULT '0' COMMENT 'Номер сущности',
-  `type` varchar(50) NOT NULL DEFAULT '' COMMENT 'Тип операции (класс)',
+  `instance` tinyint(3) NOT NULL DEFAULT '0' COMMENT 'Номер сущности',
+  `worker` varchar(50) NOT NULL DEFAULT '' COMMENT 'Тип операции (класс)',
   `function` varchar(50) NOT NULL DEFAULT '' COMMENT 'Выполняемая функция',
   `maxValue` int(10) unsigned NOT NULL COMMENT 'Максимальное время выполнения в мс',
   `minValue` int(10) unsigned NOT NULL COMMENT 'Минимальное время выполнения в мс',
   `counter` int(10) unsigned NOT NULL COMMENT 'Количество выполнений',
   `sumValue` int(10) unsigned NOT NULL COMMENT 'Общее время выполнения в мс',
-  PRIMARY KEY (`programm`,`dttm`,`worker`,`type`,`function`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Функциональный мониторинг нагрузки';
-
+  PRIMARY KEY (`dttm`,`instance`,`worker`,`function`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Функциональный мониторинг нагрузки'
+PARTITION BY LINEAR KEY (worker)
+PARTITIONS 6;
